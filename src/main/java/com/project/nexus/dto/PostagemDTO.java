fa09e146fc7cafb366427fac1af1d4a7e2356491
Allekkz <160639103+Allekkz@users.noreply.github.com>
2026-03-20 09:35:48 -0300

    Adicionando a pasta DTO

---
+++ src/main/java/com/project/nexus/dto/PostagemDTO.java
@@ -0,0 +1,26 @@
+package com.project.nexus.dto;
+
+import com.project.nexus.model.Postagem;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class PostagemDTO {
+
+    private Long id;
+    private String titulo;
+    private String conteudo;
+    private String nomeUsuario;
+    private String fotoUsuario;
+
+    public PostagemDTO(Postagem postagem) {
+        this.id = postagem.getId();
+        this.titulo = postagem.getTitulo();
+        this.conteudo = postagem.getConteudo();
+        this.nomeUsuario = postagem.getUsuario().getNome();
+        this.fotoUsuario = postagem.getUsuario().getImgUrl();
+    }
+}
