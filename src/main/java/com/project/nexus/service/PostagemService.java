3de32a5030424cdcf7a7576ae3360c6a4b747b94
Allekkz <160639103+Allekkz@users.noreply.github.com>
2026-03-20 10:18:34 -0300

    Update PostagemService.java

--- src/main/java/com/project/nexus/service/PostagemService.java
+++ src/main/java/com/project/nexus/service/PostagemService.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.project.nexus.dto.PostagemDTO;
 import com.project.nexus.model.Postagem;
 import com.project.nexus.model.Usuario;
+import com.project.nexus.repository.CurtidaRepository;
 import com.project.nexus.repository.PostagemRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class PostagemService {
 
     private final PostagemRepository postagemRepository;
+    private final CurtidaRepository curtidaRepository;
 
     /* Criar postagem: */
     public Postagem criar(Postagem postagem) {
@@ -24,18 +27,70 @@ public class PostagemService {
     }
 
     /* Listar Todas Postagens - FEED: */
-    public List<Postagem> listar() {
-        return postagemRepository.findAllByOrderByDataCriacaoDesc();
+    /*
+     * public List<Postagem> listar() {
+     * return postagemRepository.findAllByOrderByDataCriacaoDesc();
+     * }
+     */
+    /* usando postagem DTO, para mandar apenas o necessário no json: */
+    public List<PostagemDTO> listar() {
+        return postagemRepository.findAllByOrderByDataCriacaoDesc()
+                .stream()
+                .map(postagem -> {
+                    PostagemDTO dto = new PostagemDTO(postagem);
+
+                    dto.setTotalCurtidas(
+                            curtidaRepository.countByPostagem(postagem));
+
+                    return dto;
+                })
+                .toList();
     }
 
     /* Buscar postagem por id: */
-    public Optional<Postagem> buscarPorId(Long id) {
-        return postagemRepository.findById(id);
+    /*
+     * public Optional<Postagem> buscarPorId(Long id) {
+     * return postagemRepository.findById(id);
+     * }
+     */
+    /* usando postagemDTO: */
+    public Optional<PostagemDTO> buscarPorId(Long id) {
+        return postagemRepository.findById(id)
+                .map(postagem -> {
+                    PostagemDTO dto = new PostagemDTO(postagem);
+
+                    dto.setTotalCurtidas(
+                            curtidaRepository.countByPostagem(postagem));
+
+                    return dto;
+                });
     }
 
     /* Listar postagem de um usuário: */
-    public List<Postagem> listarPorUsuario(Long usuarioId) {
-        return postagemRepository.findByUsuarioIdOrderByDataCriacaoDesc(usuarioId);
+    /*
+     * public List<Postagem> listarPorUsuario(Long usuarioId) {
+     * return postagemRepository.findByUsuarioIdOrderByDataCriacaoDesc(usuarioId);
+     * }
+     */
+    /* usando PostagemDTO: */
+    public List<PostagemDTO> listarPorUsuario(Long usuarioId) {
+        return postagemRepository.findByUsuarioIdOrderByDataCriacaoDesc(usuarioId)
+                .stream()
+                .map(postagem -> {
+                    PostagemDTO dto = new PostagemDTO(postagem);
+
+                    dto.setTotalCurtidas(
+                            curtidaRepository.countByPostagem(postagem));
+
+                    return dto;
+                })
+                .toList();
+    }
+
+    /* metodo novo: */
+    public Postagem buscarEntidadePorId(Long id) {
+        return postagemRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Postagem não encontrada"));
     }
 
     /* Deletar postagem: */
@@ -43,7 +98,7 @@ public class PostagemService {
         postagemRepository.deleteById(id);
     }
 
-    /* Contar postagens */ 
+    /* Contar postagens */
     public long contarPostagens(Usuario usuario) {
         return postagemRepository.countByUsuario(usuario);
     }
